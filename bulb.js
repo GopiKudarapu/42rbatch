b7756e99d511ca1bb9bc344a2a5ea7886aeb1e87
Gopik <gopik32004@gmail.com>
2025-09-20 13:37:57 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,9 @@
+document.getElementById("title")
+
+function Switchon(){
+    document.getElementById("bulb").src="https://thumbs.dreamstime.com/b/light-bulb-black-background-17047477.jpg"
+}
+
+function Switchoff(){
+    document.getElementById("bulb").src="https://cdn.vectorstock.com/i/1000v/70/44/3d-realistic-off-light-bulb-icon-closeup-vector-27407044.jpg"
+}
